11788afcbb27e86307acd6e9f29ee865c6baf503
Auto Differentiation Dev Team <107129969+auto-differentiation-dev@users.noreply.github.com>
2024-03-01 16:12:39 +0000

    Fixes copyright year to 2024

--- src/XAD/Interface.hpp
+++ src/XAD/Interface.hpp
@@ -5,7 +5,7 @@
    This file is part of XAD, a comprehensive C++ library for
    automatic differentiation.
 
-   Copyright (C) 2010-2023 Xcelerit Computing Ltd.
+   Copyright (C) 2010-2024 Xcelerit Computing Ltd.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as published
