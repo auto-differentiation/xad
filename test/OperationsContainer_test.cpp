1a65fb2aa75cde6cef07e968eb675a4ad5380cad
imraneamri <mohammad.amri@ucdconnect.ie>
2025-04-01 14:32:52 +0100

    Restored old bad alloc test

--- test/OperationsContainer_test.cpp
+++ test/OperationsContainer_test.cpp
@@ -61,8 +61,6 @@ TEST(OperationsContainerTestHuge, throwsBadAlloc)
     auto construct_huge = [&]
     {
         auto c = xad::OperationsContainer<double, int, static_cast<std::size_t>(-1)>();
-        auto t = c.size();
-        EXPECT_THAT(t, Gt(1)) << t.to_string();
         XAD_UNUSED_VARIABLE(c);
     };
     EXPECT_THAT(construct_huge, Throws<std::bad_alloc>());
