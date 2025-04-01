71e7bd01b8494db60c3945c4114aa60123945944
imraneamri <mohammad.amri@ucdconnect.ie>
2025-04-01 13:23:33 +0100

    Using OperationsContainer after bad allocation to force error

--- test/OperationsContainer_test.cpp
+++ test/OperationsContainer_test.cpp
@@ -33,6 +33,7 @@ TEST(OperationsContainerTestHuge, throwsBadAlloc)
     auto construct_huge = [&]
     {
         auto c = xad::OperationsContainer<double, int, static_cast<std::size_t>(-1)>();
+        auto t = c.size();
         XAD_UNUSED_VARIABLE(c);
     };
     EXPECT_THAT(construct_huge, Throws<std::bad_alloc>());
