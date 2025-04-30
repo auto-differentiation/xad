ab8edefe179a0e60c1e7b66e3277439255ace5b4
imraneamri <mohammad.amri@ucdconnect.ie>
2025-04-30 15:39:43 +0100

    Small change to make PR possible

---
+++ benchmarks/MonteCarlo/main.cpp
@@ -0,0 +1,19 @@
+#include <benchmark/benchmark.h>
+
+#include <XAD/XAD.hpp>
+
+
+
+static void MonteCarlo(benchmark::State &state)
+{
+
+    typedef xad::fwd<double> mode;
+
+    
+
+
+    for (auto _ : state)
+    {
+
+    }
+}
