f1e4fa0338a9d8724a574857d208b18f21db2d3d
imraneamri <mohammad.amri@ucdconnect.ie>
2025-05-02 13:40:30 +0100

    removed last slash from absolute paths@

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -15,8 +15,8 @@ if [ "$RUN_TYPE" != "reference" ] && [ "$RUN_TYPE" != "benchmark" ]; then
   exit 1
 fi
 
-DIR="/__w/xad/xad/xad/build/benchmarks/"
-MAIN_DIR="/__w/xad/xad/main/build/benchmarks/"
+DIR="/__w/xad/xad/xad/build/benchmarks"
+MAIN_DIR="/__w/xad/xad/main/build/benchmarks"
 
 #DIR="$(pwd)/build/benchmarks" # "$(pwd)/../build/benchmarks"
 #MAIN_DIR="$(pwd)/build/benchmarks" # "$(pwd)/../../main/build/benchmarks" 
