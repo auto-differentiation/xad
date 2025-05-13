76eddbaa1df4052e204e8f2dbb8667f8d2d41166
imraneamri <mohammad.amri@ucdconnect.ie>
2025-05-13 16:16:35 +0100

    Added copyright notices

--- test/Eigen_test.cpp
+++ test/Eigen_test.cpp
@@ -1,3 +1,27 @@
+/*******************************************************************************
+
+   Tests for the compatibility and correctness of XAD with Eigen types.
+
+   This file is part of XAD, a comprehensive C++ library for
+   automatic differentiation.
+
+   Copyright (C) 2010-2025 Xcelerit Computing Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
 #define XAD_EIGEN_COMPATIBILITY
 #include <XAD/EigenCompatibility.hpp>
 
@@ -112,7 +136,7 @@ TEST(Eigen, MatrixInverseFwd)
     }
 }
 
-TEST(Eigen, MatrixMultiplication)
+TEST(Eigen, MatrixMultiplicationAdj)
 {
     using mode = xad::adj<double>;
     using tape_type = mode::tape_type;
@@ -177,7 +201,7 @@ TEST(Eigen, MatrixMultiplication)
     }
 }
 
-TEST(Eigen, MatrixTrace)
+TEST(Eigen, MatrixTraceAdj)
 {
     using mode = xad::adj<double>;
     using tape_type = mode::tape_type;
@@ -231,7 +255,7 @@ TEST(Eigen, MatrixTrace)
         }
     }
 }
-TEST(Eigen, MatrixDeterminant)
+TEST(Eigen, MatrixDeterminantAdj)
 {
     using mode = xad::adj<double>;
     using tape_type = mode::tape_type;
@@ -288,7 +312,7 @@ TEST(Eigen, MatrixDeterminant)
     }
 }
 
-TEST(Eigen, MatrixNorm)
+TEST(Eigen, MatrixNormAdj)
 {
     using mode = xad::adj<double>;
     using tape_type = mode::tape_type;
